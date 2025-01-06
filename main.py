dc3c4fdff5a3b095116fb72e6c3b6a7f92477dc0
galsabach <galsabach66@gmail.com>
2025-01-06 11:28:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-
 df = pd.read_csv('parkinsons.csv')
 df = df.dropna()
 
@@ -12,14 +7,18 @@ target = 'status'
 x = df[features]
 y = df[target]
 
+from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
 
-x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42
 
+from sklearn.svm import SVC
 model = SVC()
 model.fit(x_train, y_train)
 
+from sklearn.metrics import accuracy_score
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
