60eb3ee54262909a9cce52ddb7a691cf1a67653e
galsabach <galsabach66@gmail.com>
2025-01-08 11:04:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
 
 from sklearn.svm import SVC
 model = SVC()
